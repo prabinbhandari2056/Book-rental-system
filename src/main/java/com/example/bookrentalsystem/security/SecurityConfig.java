942d663967e72426bde713da4bfed305407b22c6
Prabin Bhandari <prabinbhandari2056@outlook.com>
2022-11-21 20:49:46 +0545

    cors configuration import class changed to cors

--- src/main/java/com/example/bookrentalsystem/security/SecurityConfig.java
+++ src/main/java/com/example/bookrentalsystem/security/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @EnableWebSecurity
@@ -63,6 +63,6 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
         corsConfig.addAllowedMethod("DELETE");
         corsConfig.addAllowedMethod("PATCH");
         urlBasedCors.registerCorsConfiguration("/**", corsConfig);
-        return new CorsFilter((CorsConfigurationSource) urlBasedCors);
+        return new CorsFilter( urlBasedCors);
     }
 }
